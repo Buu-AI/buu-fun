d16bbb2f905a4cd6a5f2c8a4f356364221a3c6d0
muadpn <muadpn434@gmail.com>
2025-04-12 22:50:47 +0530

    fix: removed sub popover for downloading model

--- src/components/generation/tool-tip-download.tsx
+++ src/components/generation/tool-tip-download.tsx
@@ -23,6 +23,50 @@ export default function ToolTipDownload({
   return (
     <Tooltip>
       <TooltipTrigger asChild>
+        {modelUrl ? (
+          <motion.a
+            download={true}
+            href={modelUrl ?? ""}
+            target="_blank"
+            //   onClick={() => handleEvent(item.type)}
+            initial="initial"
+            whileHover="hover"
+            whileTap="tap"
+            variants={buttonVariants}
+            transition={{ type: "spring", stiffness: 400, damping: 17 }}
+            // group bg-buu-button  hover:bg-white hover:shadow-none  group shadow-buu-button min-w-[30px]  rounded-md flex items-center justify-center p-1.5
+            className="group bg-buu-button pointer-events-auto hover:bg-white hover:shadow-none group shadow-buu-button min-w-[30px] rounded-md flex items-center justify-center p-1.5"
+          >
+            <motion.div
+              className="w-full h-full group-hover:text-black group-hover:fill-black"
+              transition={{ duration: 0.2 }}
+            >
+              {toolTipData.Icon}
+            </motion.div>
+          </motion.a>
+        ) : (
+          <div className="group bg-buu-button pointer-events-auto hover:bg-white hover:shadow-none group shadow-buu-button min-w-[30px] rounded-md flex items-center justify-center p-1.5">
+            <div className="w-4 flex items-center justify-center h-4">
+              <Loader2 className="animate-spin group-hover:text-black" />
+            </div>
+          </div>
+        )}
+      </TooltipTrigger>
+      <TooltipContent
+        className={cn("bg-buu-button text-primary", {
+          "ml-2": index === 0,
+          "mr-2": index === ToolTips.length - 1,
+        })}
+      >
+        <p>{toolTipData.content}</p>
+      </TooltipContent>
+    </Tooltip>
+  );
+}
+
+{
+  /* <Tooltip>
+      <TooltipTrigger asChild>
         <Popover>
           <PopoverTrigger asChild>
             <motion.div
@@ -79,5 +123,5 @@ export default function ToolTipDownload({
         <p>{toolTipData.content}</p>
       </TooltipContent>
     </Tooltip>
-  );
+//  */
 }
