0fcd5096770e2330a78a147ffdf11606fcd95211
muadpn <muadpn434@gmail.com>
2025-04-26 03:42:16 +0530

    feat: added jupitor widget

--- src/components/portfolio/buy-buu-button.tsx
+++ src/components/portfolio/buy-buu-button.tsx
@@ -2,13 +2,28 @@ import { useAppDispatch } from "@/hooks/redux";
 import { setBuyBuuDialogOpen } from "@/lib/redux/features/buu-pricing";
 import React from "react";
 import { Button } from "../ui/button";
+import { getClusterUrl } from "@/lib/solana/staking";
 
 export default function BuyBuuButton() {
   const dispatch = useAppDispatch();
   return (
     <Button
       onClick={() => {
-        dispatch(setBuyBuuDialogOpen(true));
+        window.Jupiter.init({
+          autoConnect: true,
+          displayMode: "modal",
+
+          endpoint: getClusterUrl(),
+
+          formProps: {
+            initialOutputMint: "88n8pBT6doB5rHP7WcAaG8TuVY1baWazzxAS7Bm8virt",
+            fixedInputMint: true,
+            fixedOutputMint: true,
+            swapMode: "ExactIn",
+            fixedAmount: true,
+            initialAmount: "",
+          },
+        }); // dispatch(setBuyBuuDialogOpen(true));
       }}
       className="h-[40px] w-full"
     >
