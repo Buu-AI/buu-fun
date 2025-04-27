5583ee74968c4913208b31f5068bb9d7e25881e0
muadpn <muadpn434@gmail.com>
2025-04-27 19:50:29 +0530

    feat: added roi calculator functions

--- src/components/portfolio/your-earning-new/roi-staking-card.tsx
+++ src/components/portfolio/your-earning-new/roi-staking-card.tsx
@@ -1,21 +1,58 @@
 import SwitchIcon from "@/assets/icons/change-icon";
 import Pill from "@/components/elements/pill";
-import { BorderBeam } from "@/components/ui/border-beam";
-import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
-import { Label } from "@/components/ui/label";
-import { cn } from "@/lib/utils";
-import React from "react";
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import { BorderBeam } from "@/components/ui/border-beam";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Label } from "@/components/ui/label";
+import { useGlobalStakingData } from "@/hooks/use-global-staking";
+import {
+  useBuuPricingData,
+  useTokenBalance,
+} from "@/hooks/use-pricing-history";
+import { cn, formatNumberWithFractions } from "@/lib/utils";
 import { ArrowDown } from "lucide-react";
+import { useState } from "react";
+import {
+  calculateROI,
+  calculateStakingROI,
+  ROICalculatorParams,
+} from "./roi-calcuation";
 import RoiDetailedLine from "./roi-detail-line";
 import RoiListInfo from "./roi-list-info";
 export default function RoiStakingCard() {
+  const { data } = useBuuPricingData();
+  const { data: globalStakingData } = useGlobalStakingData();
+  const { data: tokenData } = useTokenBalance();
+  const earnings = tokenData?.value?.uiAmount;
+  const aprPersentage = globalStakingData?.apr ?? 0;
+
+  const [balance, setBalance] = useState(earnings ?? 10_000_000);
+
+  const [amount, setAmount] = useState<number | null>(0);
+
+  const [stakedUpto, setStakedTo] = useState<1 | 7 | 30 | 365>(1);
+
+  const [compoundDays, setCompoundDays] = useState<1 | 7 | 14 | 30>(1);
+  const aprCreditsPercentage = 30;
+  // const { aprPercent, earnedTokens, earnedUSD, finalValueUSD, roiPercent }
+  const params: ROICalculatorParams = {
+    stakedAmount: amount ?? 0,
+    stakingPeriodDays: stakedUpto,
+    aprRewardsPercentage: 1,
+    aprCreditsPercentage: aprCreditsPercentage,
+    tokenPrice: data?.price ?? 0,
+    isCompounding: true,
+    compoundingPeriodDays: 7,
+  };
+  const { finalROI, } = calculateROI(params);
+
+
   return (
     <div className="">
       <div className="px-6">
@@ -24,14 +61,28 @@ export default function RoiStakingCard() {
           <div className="flex items-center  ">
             <div className="flex  py-1">
               <p className="pr-1 font-medium text-muted-foreground/60">$</p>
-              <input className="bg-transparent font-medium tracking-tight appearance-none text-xl outline-none " />{" "}
+              <input
+                value={amount ?? ""}
+                onChange={(e) => {
+                  const value = e.target.value;
+                  if (value === "") {
+                    setAmount(null);
+                    return;
+                  }
+                  if (!isNaN(Number(value)) && Number(value) >= 0) {
+                    setAmount(Number(value));
+                    return;
+                  }
+                }}
+                className="bg-transparent font-medium tracking-tight appearance-none text-xl outline-none "
+              />{" "}
             </div>
             <button className="w-6 h-6  flex">
               <SwitchIcon />
             </button>
           </div>
           <p className="text-sm font-medium text-muted-foreground/60">
-            412312312 <span>$BUU</span>
+            {balance} <span>$BUU</span>
           </p>
         </div>
 
@@ -56,38 +107,49 @@ export default function RoiStakingCard() {
           </p>
           <div className="flex items-center gap-4 justify-between bg-portfolio-statistics-button-background rounded-md px-2 py-2">
             <Button
-              variant={true ? "default" : "ghost"}
+              onClick={() => {
+                setStakedTo(1);
+              }}
+              variant={stakedUpto === 1 ? "default" : "ghost"}
               className={cn("w-full", {
                 "hover:text-white text-sm  text-muted-foreground/60 transition-all duration-300 ease-in-out hover:bg-buu-button-muted":
-                  false,
+                  stakedUpto !== 1,
               })}
             >
               1D
             </Button>
             <Button
-              onClick={() => {}}
-              variant={false ? "default" : "ghost"}
+              onClick={() => {
+                setStakedTo(7);
+              }}
+              variant={stakedUpto === 7 ? "default" : "ghost"}
               className={cn("w-full", {
-                "hover:text-white text-sm text-muted-foreground/60 transition-all duration-300 ease-in-out hover:bg-buu-button-muted":
-                  !false,
+                "hover:text-white text-sm  text-muted-foreground/60 transition-all duration-300 ease-in-out hover:bg-buu-button-muted":
+                  stakedUpto !== 7,
               })}
             >
               7D
             </Button>
             <Button
-              variant={false ? "default" : "ghost"}
+              onClick={() => {
+                setStakedTo(30);
+              }}
+              variant={stakedUpto === 30 ? "default" : "ghost"}
               className={cn("w-full", {
-                "hover:text-white text-sm text-muted-foreground/60 transition-all duration-300 ease-in-out hover:bg-buu-button-muted":
-                  !false,
+                "hover:text-white text-sm  text-muted-foreground/60 transition-all duration-300 ease-in-out hover:bg-buu-button-muted":
+                  stakedUpto !== 30,
               })}
             >
               30D
             </Button>
             <Button
-              variant={false ? "default" : "ghost"}
+              onClick={() => {
+                setStakedTo(365);
+              }}
+              variant={stakedUpto === 365 ? "default" : "ghost"}
               className={cn("w-full", {
-                "hover:text-white text-sm text-muted-foreground/60 transition-all duration-300 ease-in-out hover:bg-buu-button-muted":
-                  !false,
+                "hover:text-white text-sm  text-muted-foreground/60 transition-all duration-300 ease-in-out hover:bg-buu-button-muted":
+                  stakedUpto !== 365,
               })}
             >
               1Y
@@ -116,38 +178,49 @@ export default function RoiStakingCard() {
           </div>
           <div className="flex items-center gap-4 justify-between bg-portfolio-statistics-button-background rounded-md px-2 py-2">
             <Button
-              variant={true ? "default" : "ghost"}
+              onClick={() => {
+                setCompoundDays(1);
+              }}
+              variant={compoundDays === 1 ? "default" : "ghost"}
               className={cn("w-full", {
                 "hover:text-white text-sm  text-muted-foreground/60 transition-all duration-300 ease-in-out hover:bg-buu-button-muted":
-                  false,
+                  compoundDays !== 1,
               })}
             >
               1D
             </Button>
             <Button
-              onClick={() => {}}
-              variant={false ? "default" : "ghost"}
+              onClick={() => {
+                setCompoundDays(7);
+              }}
+              variant={compoundDays === 7 ? "default" : "ghost"}
               className={cn("w-full", {
-                "hover:text-white text-sm text-muted-foreground/60 transition-all duration-300 ease-in-out hover:bg-buu-button-muted":
-                  !false,
+                "hover:text-white text-sm  text-muted-foreground/60 transition-all duration-300 ease-in-out hover:bg-buu-button-muted":
+                  compoundDays !== 7,
               })}
             >
               7D
             </Button>
             <Button
-              variant={false ? "default" : "ghost"}
+              onClick={() => {
+                setCompoundDays(14);
+              }}
+              variant={compoundDays === 14 ? "default" : "ghost"}
               className={cn("w-full", {
-                "hover:text-white text-sm text-muted-foreground/60 transition-all duration-300 ease-in-out hover:bg-buu-button-muted":
-                  !false,
+                "hover:text-white text-sm  text-muted-foreground/60 transition-all duration-300 ease-in-out hover:bg-buu-button-muted":
+                  compoundDays !== 14,
               })}
             >
               14D
             </Button>
             <Button
-              variant={false ? "default" : "ghost"}
+              onClick={() => {
+                setCompoundDays(30);
+              }}
+              variant={compoundDays === 30 ? "default" : "ghost"}
               className={cn("w-full", {
-                "hover:text-white text-sm text-muted-foreground/60 transition-all duration-300 ease-in-out hover:bg-buu-button-muted":
-                  !false,
+                "hover:text-white text-sm  text-muted-foreground/60 transition-all duration-300 ease-in-out hover:bg-buu-button-muted":
+                  compoundDays !== 30,
               })}
             >
               30D
@@ -160,7 +233,7 @@ export default function RoiStakingCard() {
               <p className="pr-1 font-medium text-muted-foreground/60">$</p>
               <div className="flex items-center gap-2">
                 <p className="bg-transparent w-full font-medium tracking-tight appearance-none text-2xl outline-none">
-                  123
+                  {formatNumberWithFractions(finalROI)}
                 </p>
                 <Pill
                   size={"sm"}
