56bdf2587280bddb2ceefc8c86d0acb7968ed4c8
muadpn <muadpn434@gmail.com>
2025-04-29 18:17:49 +0530

    feat: added boards page and components

---
+++ src/app/(dashboard)/app/nfts/page.tsx
@@ -0,0 +1,20 @@
+import BoardsButtonWrapper from "@/components/boards/boards-button-wrapper";
+import BoardsCardWrapper from "@/components/boards/boards-cards-wrapper";
+import HeroHeader from "@/components/headers/boards/hero-header";
+import OpenPricingEffect from "@/components/subscriptions/OpenPricingEffect";
+export default function BoardsPage() {
+  return (
+    <main className="flex flex-col items-center justify-center  relative h-full w-full   ">
+      <div className="flex-1 flex items-center justify-center mr-[0.25vw] flex-col">
+        <HeroHeader
+          title="View your NFTs"
+          heroDescription={"6 Collectibles"}
+        />
+        {/* <HeaderQuickSelectCard /> */}
+        <BoardsCardWrapper />
+      </div>
+
+      {/* Bottom input section */}
+    </main>
+  );
+}
