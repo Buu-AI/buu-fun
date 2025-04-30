5bb1676d1a8445677c1ad0ba8871d34df6406d96
muadpn <muadpn434@gmail.com>
2025-05-01 00:28:05 +0530

    feat: added nft cards wrapper and cards

--- src/components/nfts/nft-cards-wrapper.tsx
+++ src/components/nfts/nft-cards-wrapper.tsx
@@ -1,9 +1,23 @@
-import React from 'react'
+"use client";
+
+import { CTAImages } from "@/assets/Image";
+import Image from "next/image";
+import NFTCard from "./nft-card";
+
+// import { useRouter } from "navi";
 
 export default function NFTCardsWrapper() {
+  const [imageOne] = CTAImages;
   return (
-    <div>
-      
+    <div className="flex gap-3  items-center flex-wrap justify-center mt-6">
+      <div className="grid grid-cols-3 gap-4">
+        <NFTCard collectionName="coding bro 1 2 3" />
+        <NFTCard />
+        <NFTCard />
+        <NFTCard />
+        <NFTCard />
+        <NFTCard />
+      </div>
     </div>
-  )
+  );
 }
