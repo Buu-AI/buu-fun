94c187fde1b98cdb3b8b17ce8723197dd34f9b6b
muadpn <muadpn434@gmail.com>
2025-05-14 19:52:21 +0530

    feat: added assets and styles for tool bar and downgraded react-markdown

---
+++ src/assets/icons/create-chat-nft-icon.tsx
@@ -0,0 +1,29 @@
+export default function CreateChatNftIcon() {
+  return (
+    <svg
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M19 15.1645V8.62852C19 8.23976 18.7917 7.8808 18.4541 7.68792L12.5375 4.30711C12.2044 4.11681 11.7956 4.11681 11.4625 4.30711L5.54587 7.68792C5.20832 7.8808 5 8.23975 5 8.62852V15.3708C5 15.7596 5.20832 16.1185 5.54587 16.3114L6.44302 16.824L10.3771 19.072L11.5016 19.7145C11.8133 19.8926 12.1931 19.9047 12.5155 19.7468L13.1667 19.4279"
+        stroke="#7B7D81"
+        stroke-width="1.08333"
+        stroke-linecap="round"
+      />
+      <path
+        d="M16.666 15.4287L16.666 19.9999"
+        // stroke="white"
+        stroke-width="1.08333"
+        stroke-linecap="round"
+      />
+      <path
+        d="M19 17.7139H14.3333"
+        // stroke="white"
+        stroke-width="1.08333"
+        stroke-linecap="round"
+      />
+    </svg>
+  );
+}
