d99d1b8691e6ade195035adf9edc47b254253b1b
muadpn <muadpn434@gmail.com>
2025-05-14 07:31:26 +0530

    feat: added model container for assisant tool message

---
+++ src/components/chat/assistant/generated-model-card.tsx
@@ -0,0 +1,48 @@
+import { BorderBeam } from "@/components/ui/border-beam";
+import {
+  isToolCallPending,
+  isToolCallPendingOrInProgress,
+} from "@/lib/helpers/status-checker";
+import { cn } from "@/lib/utils";
+import { MaybeString } from "@/types";
+import { TMessageStatus } from "@/types/chat/chat-types";
+import GenerationLoader from "./generation-card/generation-loader";
+import ImageViewLoader from "./generation-card/image-view-loader";
+import ModelViewWrapper from "./generation-card/model-view-wrapper";
+type TGeneratedModelCard = {
+  imageUrl: MaybeString;
+  modelUrl: MaybeString;
+  status: TMessageStatus;
+};
+
+export default function GeneratedModelCard({
+  imageUrl,
+  modelUrl,
+  status,
+}: TGeneratedModelCard) {
+  const isPending = isToolCallPending(status);
+  const isGenerating = isToolCallPendingOrInProgress(status);
+  if (isPending) {
+    return null;
+  }
+  return (
+    <div className="overflow-hidden  max-w-[clamp(250px,100%,320px)] w-full aspect-square rounded-2xl relative justify-center">
+      <GenerationLoader isGenerating={isGenerating} />
+      <ImageViewLoader imageUrl={imageUrl} isGenerating={isGenerating} />
+      <ModelViewWrapper imageUrl={imageUrl} modelUrl={modelUrl} />
+      <BorderBeam
+        transition={{
+          velocity: 1,
+        }}
+        containerClassName={cn("border-2 rounded-2xl", {
+          hidden: !isGenerating,
+        })}
+        initialOffset={0}
+        size={270}
+        colorFrom="rgba(119, 217, 253,1)"
+        colorTo="rgba(119, 217, 253,1)"
+        className="border-2 rounded-2xl z-50 relative"
+      />
+    </div>
+  );
+}
---
+++ src/components/chat/assistant/generation-card/model-view-wrapper.tsx
@@ -0,0 +1,24 @@
+import { MaybeString } from "@/types";
+import dynamic from "next/dynamic";
+
+const ModelViewer = dynamic(() => import("../../../generation/model-viewer"), {
+  // loading: () => <p>Loading...</p>,
+  ssr: false,
+});
+
+type TModelViewWrapper = {
+  modelUrl: MaybeString;
+  imageUrl: MaybeString;
+};
+
+export default function ModelViewWrapper({
+  modelUrl,
+  imageUrl,
+}: TModelViewWrapper) {
+  if (!modelUrl) return null;
+  return (
+    <div className="w-full h-full border overflow-hidden rounded-3xl">
+      <ModelViewer poster={imageUrl} src={modelUrl} />
+    </div>
+  );
+}
