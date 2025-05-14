80bb3528f8cfdf815becbec8b754be54f59ef94b
muadpn <muadpn434@gmail.com>
2025-05-14 07:36:55 +0530

    feat: added new utility methods for tool call status

---
+++ src/components/chat/types.temp.ts
@@ -0,0 +1,54 @@
+import { Media, Nft } from "@/gql/types/graphql";
+
+export type ToolRequestType =
+  | "GENERATE_NFT"
+  | "GENERATE_MODEL_FROM_TEXT"
+  | "GENERATE_MODEL_FROM_IMAGE";
+
+export type ToolRequestPriority = "LOW" | "MEDIUM" | "HIGH" | "URGENT";
+
+export type ToolRequestStatus =
+  | "PENDING"
+  | "IN_QUEUE"
+  | "IN_PROGRESS"
+  | "COMPLETED"
+  | "FAILED"
+  | "CANCELED";
+
+export type ToolRequestStatusWithUser = ToolRequestStatus | "USER";
+
+export interface ToolRequest {
+  id: string;
+  type: ToolRequestType;
+  priority: ToolRequestPriority;
+  payload: string;
+}
+
+export type MessageRole = "user" | "assistant" | "tool";
+
+export interface MessageContent {
+  text?: string;
+  media?: Media[] | null;
+  nft?: Nft | null;
+}
+
+export interface Message {
+  _id: string;
+  createdAt: string; // ISO date string
+  updatedAt: string; // ISO date string
+  teamId: string;
+  sessionId: string;
+  role: MessageRole;
+  status: ToolRequestStatus;
+  content?: MessageContent;
+  toolRequest?: ToolRequest;
+  toolCalls?: ToolRequest[];
+  credits?: number;
+}
+
+export type TempMessage = {
+  _id: string;
+  role: MessageRole;
+};
+
+export type PromptPayload = any;
--- src/lib/helpers/status-checker.tsx
+++ src/lib/helpers/status-checker.tsx
@@ -1,5 +1,7 @@
 import { PlanKeyMapper } from "@/constants/subscription/subscription-plans";
 import { TGenResponseStatus } from "../redux/features/chat-types";
+import { ToolRequestStatus } from "@/components/chat/types.temp";
+import { MessageRole } from "@/gql/types/graphql";
 
 export function isInProgress(status: TGenResponseStatus) {
   return status === "InProgress";
@@ -15,4 +17,38 @@ export function isFreePlan(plan?: string): boolean {
   if (plan === PlanKeyMapper.FREE) return true;
 
   return false;
+}
+
+export function isToolCallPending(status: ToolRequestStatus) {
+  return status === "PENDING";
+}
+
+export function isToolCallCompleted(status: ToolRequestStatus) {
+  return status === "COMPLETED";
+}
+
+export function isToolCallInProgress(status: ToolRequestStatus) {
+  return status === "IN_PROGRESS";
+}
+
+export function isToolCallPendingOrInProgress(status: ToolRequestStatus) {
+  return status === "IN_PROGRESS" || status === "PENDING";
+}
+export function isToolCallGenerating(status: ToolRequestStatus) {
+  return status === "IN_PROGRESS" || status === "IN_QUEUE" || status === "PENDING";
+}
+export function isToolCallInProgressOrCompleted(status: ToolRequestStatus) {
+  return status === "IN_PROGRESS" || status === "COMPLETED";
+}
+
+export function isRoleAssistant(role: string) {
+  return role === MessageRole.Assistant;
+}
+
+export function isRoleToolOrAssistant(role: string) {
+  return role === MessageRole.Assistant || role === MessageRole.Tool;
+}
+
+export function isRoleTool(role: string) {
+  return role === MessageRole.Tool;
 }
