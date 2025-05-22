e9710d7793bce6ced73511e4a0192e575fe9abb3
muadpn <muadpn434@gmail.com>
2025-05-22 17:14:12 +0530

    feat: added types for message events

---
+++ src/types/chat/event-source.ts
@@ -0,0 +1,66 @@
+import { Maybe, ToolRequest } from "@/gql/types/graphql";
+import { TMessageStatus } from "./chat-types";
+import { MaybeString } from "..";
+
+type BaseEvent = {
+  sessionId: string;
+  emittedAt: string;
+  type: string;
+};
+
+type MessageNewTokenEvent = BaseEvent & {
+  type: "message-new-token";
+  payload: {
+    messageId: string;
+    token: string;
+  };
+};
+
+export type MessageUpdatedEvent = BaseEvent & {
+  type: "message-updated";
+  sessionId: string;
+  emittedAt: string;
+  payload: {
+    _id: string;
+    createdAt?: string;
+    updatedAt?: string;
+    sessionId: string;
+    teamId: string;
+    role: "assistant" | "user" | "tool";
+    content?: {
+      text?: string;
+      images?: { url: MaybeString }[];
+      model?: {
+        alt: MaybeString;
+        keyS3: MaybeString;
+        size: number | null | undefined;
+        type: MaybeString;
+        url: MaybeString;
+        image?: {
+          url?: MaybeString;
+        };
+      };
+    };
+    toolRequest?: ToolRequest | null;
+    toolCalls?: [];
+    credits?: null | number;
+    nftId?: null | string;
+    status: TMessageStatus;
+  };
+};
+
+export type ChatEvents = MessageNewTokenEvent | MessageUpdatedEvent;
+
+type ChatEventMap = {
+  [E in ChatEvents as E["type"]]: E;
+};
+
+// Updated handler type to accept extended parameters per event
+export type TChatEventHandler<
+  ExtendedParams extends Partial<Record<keyof ChatEventMap, object>> = {},
+> = {
+  [K in keyof ChatEventMap]?: (
+    data: ChatEventMap[K] &
+      (K extends keyof ExtendedParams ? ExtendedParams[K] : {})
+  ) => void | unknown;
+};
