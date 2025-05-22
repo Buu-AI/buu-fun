458519cb4cf364eb02ea269a298508e80879e4d5
muadpn <muadpn434@gmail.com>
2025-05-22 17:22:24 +0530

    refactor: removed animation text as chat message is a streaming text

--- src/components/chat/chat-container.tsx
+++ src/components/chat/chat-container.tsx
@@ -95,84 +95,82 @@ export default function ChatContainer({ sessionId }: { sessionId: string }) {
             <div className="w-6 h-6 border-2 border-gray-300 border-t-blue-300 rounded-full animate-spin"></div>
           </motion.div>
         )}
-        <AnimatePresence>
-          <div className="flex gap-5 flex-col w-full mt-2 pb-12">
-            {messages.map((item) => {
-              const messageId = item.messageId;
-              const payload = item?.payload;
-              const role = item.role;
-              const sessionId = item.sessionId;
-              const credits = item.credits ?? 0;
-              const status = item.status;
-              const imageUrl = item.imageUrl;
-              const imageUrls = item.imageUrls;
-              const modelUrl = item.modelUrl;
-              const prompt = item.prompt;
-              const type = item.type;
-              const tokenized = typeof item.nftId === "string";
-              const nftId = item.nftId;
-
-              if (role === "user") {
-                console.log("IMAGE_URLS", imageUrl);
-              }
-
-              return (
-                <motion.div
-                  initial={{ scale: 1, opacity: 0, y: -100 }}
-                  animate={{
-                    y: 0,
-                    opacity: 1,
-                    transition: {
-                      type: "tween",
-                      duration: 0.5,
-                    },
-                  }}
-                  exit={{
-                    opacity: 0,
-                    transition: {
-                      duration: 0.3,
-                      ease: "easeInOut",
-                    },
-                  }}
-                  layout
-                  key={`message-${messageId}-${sessionId}`}
-                >
-                  {isRoleTool(role) ? (
-                    <AssistantToolMessage
-                      messageId={messageId}
-                      prompt={prompt}
-                      imageUrls={imageUrls}
-                      status={status}
-                      nftId={nftId}
-                      type={type}
-                      tokenized={tokenized}
-                      imageUrl={imageUrl}
-                      modelUrl={modelUrl}
-                      payload={payload}
-                      credits={credits}
-                    />
-                  ) : null}
-                  {isRoleAssistant(role) ? (
-                    <AssistantMessage
-                      isLastMessage={
-                        item.isAssistantLastMessage && !isInitialLoad.current
-                      }
-                      prompt={prompt}
-                    />
-                  ) : null}
-                  {isRoleUser(role) ? (
-                    <UserChatMessage
-                      status={status}
-                      messageId={messageId}
-                      imageUrls={imageUrls}
-                      text={prompt ?? ""}
-                    />
-                  ) : null}
-                </motion.div>
-              );
-            })}
-          </div>
-        </AnimatePresence>
+        {/* <AnimatePresence> */}
+        <div className="flex gap-5 flex-col w-full mt-2 pb-12">
+          {messages.map((item) => {
+            const messageId = item.messageId;
+            const payload = item?.payload;
+            const role = item.role;
+            const sessionId = item.sessionId;
+            const credits = item.credits ?? 0;
+            const status = item.status;
+            const imageUrl = item.imageUrl;
+            const imageUrls = item.imageUrls;
+            const modelUrl = item.modelUrl;
+            const prompt = item.prompt;
+            const createdAt = item.createdAt;
+            const type = item.type;
+            const tokenized = typeof item.nftId === "string";
+            const nftId = item.nftId;
+            return (
+              <div
+                // initial={{ scale: 1, opacity: 0, y: -100 }}
+                // animate={{
+                //   y: 0,
+                //   opacity: 1,
+                //   transition: {
+                //     type: "tween",
+                //     duration: 0.5,
+                //   },
+                // }}
+                // exit={{
+                //   opacity: 0,
+                //   transition: {
+                //     duration: 0.3,
+                //     ease: "easeInOut",
+                //   },
+                // }}
+                // layout
+                key={`message-${messageId}-${sessionId}`}
+              >
+                {isRoleTool(role) ? (
+                  <AssistantToolMessage
+                    messageId={messageId}
+                    prompt={prompt}
+                    imageUrls={imageUrls}
+                    status={status}
+                    nftId={nftId}
+                    type={type}
+                    tokenized={tokenized}
+                    imageUrl={imageUrl}
+                    modelUrl={modelUrl}
+                    payload={payload}
+                    credits={credits}
+                  />
+                ) : null}
+                {isRoleAssistant(role) ? (
+                  <AssistantMessage
+                    createdAt={createdAt}
+                    status={status}
+                    isLastMessage={
+                      item.isAssistantLastMessage && !isInitialLoad.current
+                    }
+                    prompt={prompt}
+                  />
+                ) : null}
+                {isRoleUser(role) ? (
+                  <UserChatMessage
+                    status={status}
+                    messageId={messageId}
+                    imageUrls={imageUrls}
+                    text={prompt ?? ""}
+                  />
+                ) : null}
+              </div>
+            );
+          })}
+        </div>
+        {/* </AnimatePresence> */}
       </div>
     </div>
   );
