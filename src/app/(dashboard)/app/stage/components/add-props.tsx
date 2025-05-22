4c20dd05f7785228dbf91a22f6986a0233aa031d
muadpn <muadpn434@gmail.com>
2025-05-22 05:58:12 +0530

    feat: added add props header component for lights

---
+++ src/app/(dashboard)/app/stage/components/add-props.tsx
@@ -0,0 +1,44 @@
+import { useAppDispatch, useAppSelector } from "@/hooks/redux";
+import { createDefaultLight } from "../helper/utils";
+import { addLights, removeLights } from "@/lib/redux/features/stage";
+import { LightType } from "./lights-item";
+
+type TAddProps = {};
+
+export default function AddProps({}: TAddProps) {
+  const lights = useAppSelector((state) => state.stage.lights);
+  const dispatch = useAppDispatch();
+
+  const addLight = (type: LightType) => {
+    const newLight = createDefaultLight(type);
+    dispatch(addLights(newLight));
+  };
+
+  const removeLight = (id: string) => {
+    dispatch(removeLights(id));
+  };
+  return (
+    <div className="border-2 absolute z-[100] top-0 right-0 max-w-max w-full h-full ">
+      <div className="">
+        <button
+          className="hover:bg-red-400"
+          onClick={() => addLight("directional")}
+        >
+          Add Directional Light
+        </button>
+        <button className="hover:bg-red-400" onClick={() => addLight("spot")}>
+          Add Spot Light
+        </button>
+        <ul style={{ marginTop: 10, padding: 0 }}>
+          {lights.map((l) => (
+            <li key={l.id} style={{ listStyle: "none" }}>
+              <button onClick={() => removeLight(l.id)}>
+                Remove {l.type} - {l.id}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
