6a8cb6d566789c36c4f5865f700ab6f9834666bf
muadpn <muadpn434@gmail.com>
2025-06-04 20:58:57 +0530

    feat: added new v3 chat message prepare function and loader

--- src/lib/redux/prepare/message.ts
+++ src/lib/redux/prepare/message.ts
@@ -1,62 +1,78 @@
+import { Message } from "@/gql/types/graphql";
 import { isRoleAssistant } from "@/lib/helpers/status-checker";
 import { parseJson } from "@/lib/utils";
 import {
   PromptPayload,
   TChatMessage,
   TMessageQueryData,
+  TToolRequest,
 } from "@/types/chat/chat-types";
 import { InfiniteData } from "@tanstack/react-query";
 
 export function prepareMessagePayload(
-  params: InfiniteData<TMessageQueryData>,
+  params: InfiniteData<TMessageQueryData>
 ): TChatMessage[] {
-  const data = [
-    ...new Map(
-      params.pages
-        .flatMap((page) => {
-          return page.items.map((item) => {
-            const { data: payload } = parseJson<PromptPayload>(
-              item.toolRequest?.payload ?? "",
-            );
-            const imageUrls =
-              item.content?.images
-                ?.map((image) => image.url)
-                .filter((fv) => typeof fv === "string") ?? [];
-
-            return {
-              credits: item.credits,
-              isAssistantLastMessage: false,
-              type: item.toolRequest?.type,
-              messageId: item._id,
-              sessionId: item.sessionId,
-              createdAt: item.createdAt,
-              imageUrl: item.content?.model?.image.url,
-              imageUrls: imageUrls,
-              modelUrl: item.content?.model?.url,
-              nftId: item.nftId,
-              prompt: item.content?.text,
-              role: item.role,
-              status: item.status,
-              teamId: item.teamId,
-              payload,
-            };
-          });
-        })
-        .map((item) => [item.messageId, item]), // Use messageId as the key
-    ).values(),
-  ].sort(
+  console.log("RECEIVED PARAMS:", params);
+  const data = TransformMessages({ params }).sort(
     (a, b) =>
       new Date(a.createdAt as string).getTime() -
-      new Date(b.createdAt as string).getTime(),
+      new Date(b.createdAt as string).getTime()
   );
-
   const reversedArray = [...data];
   const lastAssistantMessage = reversedArray.findIndex((item) =>
-    isRoleAssistant(item.role),
+    isRoleAssistant(item.role)
   );
+
   if (lastAssistantMessage !== -1) {
     const arrIndex = reversedArray.length - 1 - lastAssistantMessage;
     data[arrIndex]["isAssistantLastMessage"] = true;
   }
+  console.log("SELECTED:", data);
   return data;
+}
+
+export function TransformMessages({
+  params,
+}: {
+  params: InfiniteData<TMessageQueryData>;
+}) {
+  return [
+    ...new Map(
+      params.pages
+        .flatMap((page) => {
+          return page.items.map((item) => TransformMessage(item));
+        })
+        ?.map((item) => [item.messageId, item])
+    ).values(),
+  ];
+}
+
+export function TransformMessage(item: Message) {
+  const { data: payload } = parseJson<PromptPayload>(
+    item.toolRequest?.payload ?? ""
+  );
+  console.log("payload:", payload);
+  const toolRequest: TToolRequest | undefined = item?.toolRequest
+    ? {
+        ...item?.toolRequest,
+        payload: payload,
+      }
+    : undefined;
+
+  const chatMessage: TChatMessage = {
+    medias: item.content?.medias,
+    isAssistantLastMessage: false,
+    models: item.content?.models ?? [],
+    messageId: item._id,
+    sessionId: item.sessionId,
+    createdAt: item.createdAt,
+    prompt: item.content?.text,
+    role: item.role,
+    teamId: item.teamId,
+    payload,
+    toolRequest: toolRequest,
+    credits: toolRequest?.credits,
+    status: toolRequest?.status,
+  };
+  return chatMessage;
 }
--- src/lib/redux/selectors/chatMessages.ts
+++ src/lib/redux/selectors/chatMessages.ts
@@ -5,6 +5,7 @@ import { createSelector } from "@reduxjs/toolkit";
 const Messages = (state: RootState) => state.chat.messages;
 export const isChatGenerating = createSelector([Messages], (messages) => {
   return messages.some((message) => {
+    if (!message.status) return false;
     return isToolCallGeneratingOrPending(message.status);
   });
 });
