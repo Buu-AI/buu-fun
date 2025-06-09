b0cba841cc430e0f8b30413618b24806e3acecda
muadpn <muadpn434@gmail.com>
2025-06-10 00:57:06 +0530

    feat: added new config for sentry and global-error.ts file

--- next.config.ts
+++ next.config.ts
@@ -75,7 +75,9 @@ export default withSentryConfig(withMDX(nextConfig), {
 
   // Upload a larger set of source maps for prettier stack traces (increases build time)
   widenClientFileUpload: true,
-
+  reactComponentAnnotation: {
+    enabled: true,
+  },
   // Route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
   // This can increase your server load as well as your hosting bill.
   // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
