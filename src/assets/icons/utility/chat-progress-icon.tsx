47c4b4e3fdf880b5f3bd062bf9f8247709ba8036
muadpn <muadpn434@gmail.com>
2025-06-19 19:58:59 +0530

    feat: added new icon and loader svgs

---
+++ src/assets/icons/utility/chat-progress-icon.tsx
@@ -0,0 +1,16 @@
+type TChatProgressIcon = {};
+
+export default function ChatProgressIcon({}: TChatProgressIcon) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 25" fill="none">
+      <circle
+        cx="11.9471"
+        cy="12.4998"
+        r="10.3578"
+        stroke="#FFFA78"
+        strokeWidth="2.37105"
+        strokeDasharray="4.74 4.74"
+      />
+    </svg>
+  );
+}
