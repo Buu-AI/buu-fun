bc0a4d322e0d16308c28d0c3b7c10b221205b338
muadpn <muadpn434@gmail.com>
2025-06-19 20:01:46 +0530

    feat: added new loader card for generated model

--- src/components/chat/assistant/generated-model-card.tsx
+++ src/components/chat/assistant/generated-model-card.tsx
@@ -5,12 +5,16 @@ import {
   isToolCallPending,
 } from "@/lib/helpers/status-checker";
 import { cn } from "@/lib/utils";
-import { MaybeString } from "@/types";
-import { TMessageStatus, TToolType } from "@/types/chat/chat-types";
+import { Maybe, MaybeString } from "@/types";
+import { TMessageStatus, TToolRequest, TToolType } from "@/types/chat/chat-types";
 import MagicLoaderWand from "../magic-loader-wand";
 import ImageViewLoader from "./generation-card/image-view-loader";
 import ModelViewWrapper from "./generation-card/model-view-wrapper";
 import ModelToolHeader from "./model-tool-header";
+import LoaderCircle from "../Loader-circle";
+import { Search, ZoomIn } from "lucide-react";
+import ViewModelTrigger from "./view-model-trigger";
+import { Model } from "@/gql/types/graphql";
 type TGeneratedModelCard = {
   imageUrl: MaybeString;
   modelUrl: MaybeString;
@@ -23,6 +27,9 @@ type TGeneratedModelCard = {
   toolPercentage?: number;
   modelId: MaybeString;
   isTexturedMesh: boolean;
+  index?: number;
+  model: Model;
+  toolRequest: Maybe<TToolRequest>;
 };
 
 export default function GeneratedModelCard({
@@ -36,55 +43,44 @@ export default function GeneratedModelCard({
   toolPercentage,
   modelId,
   isTexturedMesh,
+  index,
 }: TGeneratedModelCard) {
   const isGenerating = isToolCallGenerating(status);
   const isPending = isToolCallPending(status);
   const isCanceledMessage = isToolCallCanceled(status);
-
-  if (isPending || isCanceledMessage) return null;
-
   return (
-    <div className="mt-3">
-      <ModelToolHeader
-        message={message}
-        percentage={toolPercentage}
-        status={status}
-        isGenerating={isGenerating}
+    <div className=" w-full aspect-square overflow-hidden rounded-2xl  relative justify-center">
+      <div className="w-6 h-6 text-gray-400 absolute top-2 z-20 right-2">
+        <ViewModelTrigger />{" "}
+      </div>
+      <div
+        className={cn(
+          " w-full h-full   image-model-generation transition-all duration-300 ease-in-out  absolute top-0 left-0 ",
+          {
+            "image-loader": isPending,
+          }
+        )}
       />
-      <div className="max-w-[clamp(250px,100%,320px)] bg-balance-card overflow-hidden  w-full aspect-square rounded-b-2xl relative justify-center">
-        {!modelUrl ? (
-          <div className="w-full h-full  absolute top-0 left-0 z-20">
-            <div className="flex items-center justify-center  h-full w-full">
-              <MagicLoaderWand />
-            </div>
+      {!modelUrl ? (
+        <div className="w-full h-full  absolute top-0 left-0 z-20">
+          <div className="flex items-center justify-center w-full h-full  ">
+            {isPending ? (
+              <p className="text-2xl text-center ">
+                Approval
+                <br />
+                Pending
+              </p>
+            ) : (
+              <div className="h-32 w-32 ">
+                <LoaderCircle index={index} />
+              </div>
+            )}
           </div>
-        ) : null}
-        <ImageViewLoader imageUrl={null} isGenerating={isGenerating} />
-
-        <ModelViewWrapper imageUrl={null} modelUrl={modelUrl} />
-      </div>
-      <div className="h-[20px]">
-        <div
-          className={cn("flex mt-2 gap-2 pl-3", {
-            hidden: isGenerating || !modelUrl,
-          })}
-        >
-          <ToolBarWrapper
-            type="model"
-            imageId={null}
-            imageUrl={imageUrl}
-            modelUrl={modelUrl}
-            messageId={messageId}
-            nftId={nftId}
-            tokenized={tokenized}
-            modelId={modelId}
-            disabled={{
-              GENERATE_MODEL: true,
-              GENERATE_NFT: !isTexturedMesh,
-            }}
-          />
         </div>
-      </div>
+      ) : null}
+      <ImageViewLoader imageUrl={null} isGenerating={isGenerating} />
+
+      <ModelViewWrapper imageUrl={null} modelUrl={modelUrl} />
     </div>
   );
 }
