4f2563ec30cd39af832af2d2e0fda501a3a74c11
muadpn <muadpn434@gmail.com>
2025-06-21 01:36:06 +0530

    feat: added icons and assets for new v3 Chat-UI

--- src/assets/icons/cross-failed.tsx
+++ src/assets/icons/cross-failed.tsx
@@ -1,12 +1,12 @@
-type TCrossFailed = {};
+type TCrossFailed = { stroke?: string };
 
-export default function CrossFailed({}: TCrossFailed) {
+export default function CrossFailed({ stroke = "#EE2525" }: TCrossFailed) {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 18 19" fill="none">
-      <circle cx="9" cy="9.5" r="8" stroke="#EE2525" strokeWidth="1.83133" />
+      <circle cx="9" cy="9.5" r="8" stroke={stroke} strokeWidth="1.83133" />
       <path
         d="M6.2 6.7L11.8 12.3M11.8 6.7L6.2 12.3"
-        stroke="#EE2525"
+        stroke={stroke}
         strokeWidth="1.8"
         strokeLinecap="round"
         strokeLinejoin="round"
