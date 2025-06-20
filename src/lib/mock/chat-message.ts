7ed248d74ebf64958ac9cce5091ee6da78aa9a96
muadpn <muadpn434@gmail.com>
2025-06-21 01:54:26 +0530

    feat: added new event source polyfill

--- src/lib/mock/chat-message.ts
+++ src/lib/mock/chat-message.ts
@@ -1,5 +1,2 @@
-import mockOne from "./mock1.json";
-import mockTwo from "./mock2.json";
-import mockThree from "./mock3.json";
 import mockFour from "./mock4.json";
 export const mockData = mockFour;
