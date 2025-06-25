30f89a134e4ec27b63245832f643a504012a1d42
muadpn <muadpn434@gmail.com>
2025-06-26 02:54:15 +0530

    feat: added new chat option settings styles and contents

--- src/components/chat-input/chat-form.tsx
+++ src/components/chat-input/chat-form.tsx
@@ -1,10 +1,8 @@
 "use client";
 import { ArrowUp, ImageIcon } from "@/assets/icons";
+import { TypedAppError } from "@/class/error";
 import { useAppDispatch, useAppSelector } from "@/hooks/redux";
 import { getPresignedUrl } from "@/lib/react-query/image-upload";
-import { motion } from "framer-motion";
-// import { generateSubThreads } from "@/lib/react-query/threads";
-import { TypedAppError } from "@/class/error";
 import { sendChatMessage } from "@/lib/react-query/threads.v3";
 import {
   clearInput,
@@ -22,7 +20,7 @@ import { blobUrlToFile, cn, getAllowedContentTypeMaps } from "@/lib/utils";
 import { useAuthentication } from "@/providers/account.context";
 import { nanoid } from "@reduxjs/toolkit";
 import { useMutation } from "@tanstack/react-query";
-import { AnimatePresence } from "framer-motion";
+import { AnimatePresence, motion } from "framer-motion";
 import { Loader2 } from "lucide-react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
@@ -79,7 +77,13 @@ export default function ChatForm({ action }: TBottomBarContainer) {
   });
 
   const fileCount = useAppSelector((state) => state.chat.inputFile.length);
-
+  const {
+    faces,
+    ThreeDStyle,
+    textureType,
+    numberOfModels,
+    numberOfModelsMode,
+  } = useAppSelector((state) => state.settings);
   // const queryClient = useQueryClient();
   // mutation for existing chat
   const { mutate: createExistingChat, isPending: isExistingChatPending } =
@@ -128,7 +132,7 @@ export default function ChatForm({ action }: TBottomBarContainer) {
 
   const handleImageUploadUrl = async (
     ImageData: TImageData,
-    accessToken: string,
+    accessToken: string
   ) => {
     try {
       const file = await blobUrlToFile(ImageData.url, ImageData.name);
@@ -204,7 +208,7 @@ export default function ChatForm({ action }: TBottomBarContainer) {
 
       if (inputFile && inputFile?.length > 0) {
         const inputFileRequests = inputFile.map((item) =>
-          handleImageUploadUrl(item, identityToken),
+          handleImageUploadUrl(item, identityToken)
         );
         toast.loading("Preparing image for uploading", { duration: 1200 });
 
@@ -229,7 +233,7 @@ export default function ChatForm({ action }: TBottomBarContainer) {
           return;
         }
       }
-
+    
       // Handle based on action type
       if (action === "new_chat") {
         const sessionId = uuid();
@@ -237,16 +241,28 @@ export default function ChatForm({ action }: TBottomBarContainer) {
           sessionId,
           accessToken: identityToken ?? "",
           prompt: prompt,
-          style: style,
           imageUrls,
+          options: {
+            numberOfFaces: faces,
+            style: ThreeDStyle,
+            texture: textureType,
+            numberOfModels:
+              numberOfModelsMode !== "definedByAI" ? numberOfModels : null,
+          },
         });
       } else if (typeof action !== "string") {
         createExistingChat({
           accessToken: identityToken ?? "",
           prompt,
-          style,
           sessionId: action.sessionId,
           imageUrls,
+          options: {
+            numberOfFaces: faces,
+            numberOfModels:
+              numberOfModelsMode !== "definedByAI" ? numberOfModels : null,
+            style: ThreeDStyle,
+            texture: textureType,
+          },
         });
       }
     } catch (error) {
@@ -271,7 +287,7 @@ export default function ChatForm({ action }: TBottomBarContainer) {
         "relative flex-col gap-1 flex items-start w-full p-4  mb-2  rounded-[20px]  shadow-buu-inner bg-buu",
         {
           // "p-0": !inputFile?.url.length
-        },
+        }
       )}
     >
       <AnimatePresence mode="popLayout">
--- src/components/settings/settings-bar.tsx
+++ src/components/settings/settings-bar.tsx
@@ -1,23 +1,29 @@
+"use client";
+import { cn } from "@/lib/utils";
 import SuggestionBar from "../chat-input/suggestion-bar";
 import RecentChatButton from "./recent-chat-button";
+import SettingsBarPopOver from "./settings-bar-pop-over-button";
+import SettingsBarDisplayTitle from "./settings-bar-selected-display-title";
 export default function SettingsBar() {
   return (
-    <div className="flex group relative  items-center justify-between mb-2">
-      <div className="ml-3 pb-0 px-2 flex  self-end">
+    <div className="flex group   relative items-center justify-between mb-2">
+      <div className="ml-0 pb-0 px-2  flex self-end">
         <SuggestionBar />
         <RecentChatButton />
       </div>
-      {/* <div className="px-4  rounded-2xl py-2  border items-center relative justify-center mr-4 flex gap-2 bg-buu shadow-buu-inner">
-        <PopoverHiddenTrigger />
+      <div
+        className={cn(
+          "px-4 self-end max-w-[460px]  md:w-full   transition-all duration-300 ease-in-out  rounded-2xl py-2  z-10 border  items-center relative justify-end mr-1 flex gap-2 bg-buu shadow-buu-inner flex-shrink"
+        )}
+      >
         <SettingsBarDisplayTitle />
-        <div className="px-1 py-1 shadow-buu-inner bg-buu rounded-lg flex items-center  justify-center">
-          <SelectedStyles />
-        </div>
         <div className="flex items-center  justify-center">
-          <div className="h-6  my-auto  bg-white w-[2px] bg-buu shadow-buu-inner " />
+          <div className="h-6 my-auto bg-white w-[2px] bg-buu shadow-buu-inner " />
           <SettingsBarPopOver />
         </div>
-      </div> */}
+      </div>
     </div>
   );
 }
+
+//
