bc42b3eea139203ee9ce6636384f18a0e583c1da
muadpn <muadpn434@gmail.com>
2025-07-10 01:06:40 +0530

    feat: added sculpter editor

---
+++ src/app/(dashboard)/app/(without-top-navigation)/sculpter/page.tsx
@@ -0,0 +1,11 @@
+import { EDITOR_URL } from "@/config";
+
+export default function Page() {
+  return (
+    <main className="relative h-full w-full">
+      <div className=" w-full h-full overflow-hidden rounded-lg">
+        <iframe className="w-full h-full" src={EDITOR_URL} />
+      </div>
+    </main>
+  );
+}
